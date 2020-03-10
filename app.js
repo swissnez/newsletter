1e11176ecfe507de180a5db433f79a2db84b5f13
Neal Palmer <neal@abovebeyondtech.se>
2020-03-10 19:03:46 +0100

    initial

---
+++ app.js
@@ -0,0 +1,31 @@
+//jshint esversion:6
+const express = require("express"),
+    bodyParser = require("body-parser"),
+    request = require("request");
+
+const port = 7000; // !!!!!!!!! IMPORT when using inconjuntion with LiverServer extention!!!! 
+const app = express();
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+
+
+app.get("/",function(req,res){
+    res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/",function(req,res){
+
+    let firstName = req.body.firstName;
+    let lastName = req.body.lastName;
+    let email = req.body.email;
+
+    let JSONDump = JSON.stringify(req.body);
+    res.send(JSON.stringify(req.body));
+});
+
+app.listen(port,function(){
+    console.log(`Server Port: ${port}`);
+});
+
+
