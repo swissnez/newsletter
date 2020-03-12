88e01846a66ce67a72f0c32115a8e0423cdb1afc
Neal Palmer <neal@abovebeyondtech.se>
2020-03-12 19:24:29 +0100

    https module

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 //jshint esversion:6
 const express = require("express"),
     bodyParser = require("body-parser"),
-    request = require("request");
+    request = require("request"),
+    https = require("https");
 
 
-
+const url = "";
 const listID = "cb7122f618";
 const port = 7000; // !!!!!!!!! IMPORT when using inconjuntion with LiverServer extention!!!! 
 const app = express();
@@ -12,6 +13,9 @@ const app = express();
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 
+app.listen(port,function(){
+    console.log(`Server Port: ${port}`);
+});
 
 app.get("/",function(req,res){
     res.sendFile(__dirname + "/signup.html");
@@ -48,11 +52,15 @@ app.post("/",function(req,res){
     let JSONFlatPack = JSON.stringify(data);
     res.send(JSONFlatPack);
 
+
+
+    https.request(url,options,function(response){
+
+    });
+
 });
 
-app.listen(port,function(){
-    console.log(`Server Port: ${port}`);
-});
+
 
 
 // API key 
